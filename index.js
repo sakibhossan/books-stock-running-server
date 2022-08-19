106a14bacb645ffdbeb198e6adc2d7a25aabf1af
sakib213 <sakibhss438@gmail.com>
2022-08-20 00:23:53 +0600

    create a jwt token and server secure and 401 and403 set up and run access token login system

--- index.js
+++ index.js
@@ -3,10 +3,33 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 // this is middleware function start////
 app.use(cors());
 app.use(express.json());
+
+
+// /function of jwt////
+function verifyToken(req,res,next){
+  const accessHeader = req.headers.authorization;
+  if(!accessHeader){
+    return res.status(401).send({message: 'UnAuthorized access'});
+  }
+  const token = accessHeader.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN, (err, decoded) =>{
+    if(err){
+      return res.status(403).send({message: 'Forbidden access'});
+
+    }
+    
+    req.decoded = decoded;
+    next();
+  })
+  
+  
+}
+
 
 
 const uri = `mongodb+srv://${process.env.BS_USER}:${process.env.BS_PASSWORD}@cluster0.wnhxcgw.mongodb.net/?retryWrites=true&w=majority`;
@@ -39,11 +62,34 @@ async function run() {
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
+
+    // Used to collectOrder/////
+    app.get('/collectOrder',verifyToken,async(req,res) =>{
+     const emailDecoded = req.decoded.email;
+      const email = req.query.email;
+   if(email === emailDecoded) {
+    const query ={email:email};
+    const cursor = OrderProductCollection.find(query);
+    const myOrders = await cursor.toArray();
+    res.send(myOrders);
+   }
+   else{
+    res.status(403).send({message: 'foirbidden access'})
+   }
+    })
     app.post('/collectOrder',async(req, res) =>{
       const newOrder = req.body;
       const result = await OrderProductCollection.insertOne(newOrder);
       res.send(result);
-    })
+    });
+
+
+    // Token related work///
+  app.post('/getToken',async(req,res)=>{
+    const user = req.body;
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '1d'});
+    res.send({accessToken});
+  })
    
   
    

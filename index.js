b89195107b2dc304b077df82c4b8dc13b332317d
sakib213 <sakibhss438@gmail.com>
2022-08-20 22:03:25 +0600

    all set up clear

--- index.js
+++ index.js
@@ -102,7 +102,23 @@ async function run() {
       const result = await productCollection.deleteOne(query);
       res.send(result);
 
-    })
+    });
+// app.delete('collectOrder/:id', async(req,res) =>{
+//   const id = req.params.id;
+//   const query = {_id:ObjectId(id)};
+//   const result = await OrderProductCollection.deleteOne(query);
+//   res.send(result);
+
+// })
+app.delete("/collectOrder/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id:ObjectId(id) };
+  const result = await OrderProductCollection.deleteOne(query);
+  res.send(result);
+});
+
+
+
   }
   finally {
 
